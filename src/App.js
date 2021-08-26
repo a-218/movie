9f03a810439ee1e10b093966c9d45b5f1fab5fa3
Alex <lialex0218@gmail.com>
2021-08-26 12:27:26 -0700

    finish the counter exercise

--- src/App.js
+++ src/App.js
@@ -1,13 +1,84 @@
 import React, { Component } from 'react';
 import Movies from './components/movies';
 import Counters from './components/counters'
+import NavBar from './components/navbar';
 class App extends Component {
+  state = {
+    counters: [
+      {
+        id: 1,
+        value: 0,
+      },
+      {
+        id: 2,
+        value: 0,
+      },
+      {
+        id: 3,
+        value: 0,
+      },
+      {
+        id: 4,
+        value: 0,
+      },
+    ],
+  };
+
+  constructor() {
+    super();
+    console.log('app Constructor',);
+    //this.state = this.props.something;
+
+  }
+
+  componentDidMount() {
+    // perfect place for ajax call()
+    console.log('app Constructor',);
+  }
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleDelete = (counteId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counteId);
+    this.setState({ counters: counters });
+  };
+
+  handleDecrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value--;
+    this.setState({ counters });
+  }
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+
+      return c;
+    });
+    this.setState({ counters: counters });
+  };
 
   render() {
     return (
-      <main className="container">
-        <Counters />
-      </main>
+      <React.Fragment>
+        <NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length} />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+            onDecrement={this.handleDecrement} />
+        </main>
+      </React.Fragment>
     );
   }
 }
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,21 +1,47 @@
 import React, { Component } from "react";
 
 export default class Counter extends Component {
+  componentDidUpdate(prevProps, prevState) {
+    console.log("prev props", prevProps);
+    console.log("prev state", prevState);
+    if (prevProps.counter.value !== this.props.counter.value) {
+      //ajax call and get new data
+    }
+  }
+
+  componentWillUnmount() {
+    console.log("counter unmount");
+  }
   render() {
+    console.log("Counter - rendered");
     return (
-      <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button onClick={() => this.props.onIncrement(this.props.counter)}>
-          Increment
-        </button>
-        <button
-          onClick={() => {
-            this.props.onDelete(this.props.counter.id);
-          }}
-          className="btn btn-danger btn-sm m-2"
-        >
-          Delete
-        </button>{" "}
+      <div className="row">
+        <div className="col-1">
+          <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        </div>
+        <div className="col">
+          <button
+            className="btn btn-secondary btn-sm "
+            onClick={() => this.props.onIncrement(this.props.counter)}
+          >
+            +
+          </button>
+          <button
+            className="btn btn-secondary btn-sm m-2"
+            onClick={() => this.props.onDecrement(this.props.counter)}
+            disabled={this.props.counter.value === 0 ? "diabled" : ""}
+          >
+            -
+          </button>
+          <button
+            onClick={() => {
+              this.props.onDelete(this.props.counter.id);
+            }}
+            className="btn btn-danger btn-sm"
+          >
+            x
+          </button>{" "}
+        </div>
       </div>
     );
   }
